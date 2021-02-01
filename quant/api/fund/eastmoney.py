49e38a3297c8856924c38a5e7c6730fd46908100
brianlan <brianlanbo@gmail.com>
2021-02-01 14:41:23 +0800

    added package struct

--- quant/api/fund/eastmoney.py
+++ quant/api/fund/eastmoney.py
@@ -1,8 +1,14 @@
+from typing import List
+
+from ...struct.base import Entity
+
+
 class EastMoney:
     def __init__(self):
         pass
     
-    def get_all_entities(self):
+    def get_all_entities(self) -> List[Entity]:
         pass
     
-    
+    def get_entity_by_id(self, eid: int):
+        pass
