aa32968297760502a2046a2500218e26839b77ea
brianlan <brianlanbo@gmail.com>
2021-02-02 14:21:06 +0800

    added namedtuple FundRecord

--- quant/api/fund/eastmoney.py
+++ quant/api/fund/eastmoney.py
@@ -1,6 +1,8 @@
 from typing import List, Tuple
 
 
+from ...struct.base import FundRecord
+
 class EastMoney:
     def __init__(self):
         pass
@@ -8,5 +10,5 @@ class EastMoney:
     def get_all_fund_ids(self) -> List[str]:
         pass
     
-    def get_history_by_fund_id(self, fund_id: str) -> List[Tuple]:
+    def get_history_by_fund_id(self, fund_id: str) -> List[FundRecord]:
         pass
---
+++ quant/struct/base.py
@@ -0,0 +1,4 @@
+from collections import namedtuple
+
+
+FundRecord = namedtuple("FundRecord", "date net_value cum_net_value")
